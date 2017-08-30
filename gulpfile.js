a7a5863431a74aacdbdc522379d0347d2bb2f98b
Dan Phillips <dlphillips@gmail.com>
2017-08-30 17:14:13 -0400

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+const gulp = require('gulp'),
+	sass   = require('gulp-sass');
+
+gulp.task('styles', () => {
+	return gulp.src('sass/styles.scss')
+	.pipe(sass())
+	.pipe(gulp.dest('./css'));
+});
+
